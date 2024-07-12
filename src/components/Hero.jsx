40506a8da16a4bae3ddab4f03ff39a4ef14bcb6d
sandalpatidar <sandal.patidar.0079@gmail.com>
2024-07-12 14:06:55 +0530

    chnages

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       <div className="container relative" ref={parallaxRef}>
         <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
           <h1 className="h1 mb-6">
-            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+          Unlock the Potential of &nbsp;AI&nbsp;Chatting with {` `}
             <span className="inline-block relative">
             Techno{" "}
               <img
@@ -36,8 +36,8 @@ const Hero = () => {
             </span>
           </h1>
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            Unleash the power of AI within Techno. Upgrade your productivity
-            with Techno, the open AI chat app.
+            Unleash the power of AI within Techno.  
+            The Open Chat App for Enhanced Productivity and Business Growth.
           </p>
           <Button href="/pricing" white>
             Get started
